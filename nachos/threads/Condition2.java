a2119f1421bb2dde4395726e706b60a5f7047f10
ideaPeng <ideaPeng@163.com>
2015-11-13 02:27:18 +0800

    proj1 task1&task2

--- nachos/threads/Condition2.java
+++ nachos/threads/Condition2.java
@@ -9,50 +9,77 @@ import nachos.machine.*;
  * <p>
  * You must implement this.
  *
- * @see	nachos.threads.Condition
+ * @see nachos.threads.Condition
  */
 public class Condition2 {
-    /**
-     * Allocate a new condition variable.
-     *
-     * @param	conditionLock	the lock associated with this condition
-     *				variable. The current thread must hold this
-     *				lock whenever it uses <tt>sleep()</tt>,
-     *				<tt>wake()</tt>, or <tt>wakeAll()</tt>.
-     */
-    public Condition2(Lock conditionLock) {
-	this.conditionLock = conditionLock;
-    }
+	/**
+	 * Allocate a new condition variable.
+	 *
+	 * @param conditionLock
+	 *            the lock associated with this condition variable. The current
+	 *            thread must hold this lock whenever it uses <tt>sleep()</tt>,
+	 *            <tt>wake()</tt>, or <tt>wakeAll()</tt>.
+	 */
+	public Condition2(Lock conditionLock) {
+		this.conditionLock = conditionLock;
+		waitQueue = ThreadedKernel.scheduler.newThreadQueue(false);
+	}
 
-    /**
-     * Atomically release the associated lock and go to sleep on this condition
-     * variable until another thread wakes it using <tt>wake()</tt>. The
-     * current thread must hold the associated lock. The thread will
-     * automatically reacquire the lock before <tt>sleep()</tt> returns.
-     */
-    public void sleep() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+	/**
+	 * Atomically release the associated lock and go to sleep on this condition
+	 * variable until another thread wakes it using <tt>wake()</tt>. The current
+	 * thread must hold the associated lock. The thread will automatically
+	 * reacquire the lock before <tt>sleep()</tt> returns.
+	 */
+	public void sleep() {
+		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		
+		boolean status = Machine.interrupt().disable();
+		
+		conditionLock.release();
+		waitQueue.waitForAccess(KThread.currentThread());
+		KThread.currentThread().sleep();//失去锁的线程sleep，亦即阻塞，如果有其他线程wake本线程，则向下进行
+		conditionLock.acquire();//睡眠线程重新获得锁
+		
+		Machine.interrupt().restore(status);
+	}
 
-	conditionLock.release();
+	/**
+	 * Wake up at most one thread sleeping on this condition variable. The
+	 * current thread must hold the associated lock.
+	 */
+	public void wake() {
+		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		
+		boolean status = Machine.interrupt().disable();
+		KThread thread = waitQueue.nextThread();
+		
+		if(thread != null) {
+			thread.ready();//把要唤醒的线程放入等待队列
+		}
+		
+		Machine.interrupt().restore(status);
+		
+	}
 
-	conditionLock.acquire();
-    }
+	/**
+	 * Wake up all threads sleeping on this condition variable. The current
+	 * thread must hold the associated lock.
+	 */
+	public void wakeAll() {
+		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		
+		boolean status = Machine.interrupt().disable();
+		
+		KThread thread = waitQueue.nextThread();
+		while(thread != null){
+			thread.ready();
+			thread = waitQueue.nextThread();
+		}
+		
+		Machine.interrupt().restore(status);
+	}
 
-    /**
-     * Wake up at most one thread sleeping on this condition variable. The
-     * current thread must hold the associated lock.
-     */
-    public void wake() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-    }
-
-    /**
-     * Wake up all threads sleeping on this condition variable. The current
-     * thread must hold the associated lock.
-     */
-    public void wakeAll() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-    }
-
-    private Lock conditionLock;
+	private Lock conditionLock;
+	private ThreadQueue waitQueue = null;
 }
